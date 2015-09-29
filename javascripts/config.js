0379f417e5a3d496f6b2196d298b451ad754dffb
lit <seadog@yeah.net>
2015-09-29 19:51:05 +0800

    Initial

---
+++ javascripts/config.js
@@ -0,0 +1,18 @@
+var disqus_shortname = 'lis10';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://doget.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'doget';						//Your username of GitHub.
+var repos = 'doget.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'doget';						//Name of your blog.
+var rss = '';									//Rss address of your blog.
+var suffix = '';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+
+//Sample Config:
+//var disqus_shortname = 'sneezry';
+//var hostbase = 'http://sneezry.com/blog';
+//var githubname = 'sneezry';
+//var repos = 'blog';	
+//var sitetitle = 'Sneezry';
+//var rss = '';
+//var suffix = '.md';
+//var branch = 'gh-pages';
